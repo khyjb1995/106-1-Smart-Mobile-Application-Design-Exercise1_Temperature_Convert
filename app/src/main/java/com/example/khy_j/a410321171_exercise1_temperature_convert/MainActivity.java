c44af840d86681211f7f2a55cca6379821110221
khyjb1995 <yong950423@stu.xawl.edu.cn>
2017-10-17 16:40:05 +0800

    20172017

--- app/src/main/java/com/example/khy_j/a410321171_exercise1_temperature_convert/MainActivity.java
+++ app/src/main/java/com/example/khy_j/a410321171_exercise1_temperature_convert/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnCtoF, btnCtoK, btnKtoF, btnKtoC, btnFtoC, btnFtoK;
     private TextView txtResult, txtPastResult;
+    private String passrec, result;
     private EditText txtInput;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,35 +41,46 @@ public class MainActivity extends AppCompatActivity {
     private Button.OnClickListener myListener =new Button.OnClickListener(){
         @Override
         public void onClick(View view) {
-            txtPastResult.setText(txtResult.getText().toString() + "\n" + txtPastResult.getText().toString());
             Double s = Double.parseDouble(txtInput.getText().toString()) ;
+            if(txtResult.getText().toString() != "")
+            {
+                txtPastResult.setText(passrec + "\n" + txtPastResult.getText().toString());
+            }
+            passrec = s.toString();
             switch (view.getId()) {
                 case R.id.btnCtoF: {
                     s = s * 1.8 + 32;
+                    result = "°C  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°F";
                     break;
                 }
                 case R.id.btnCtoK: {
                     s = s + 273.15;
+                    result = "°C  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + " K";
                     break;
                 }
                 case R.id.btnFtoC:{
                     s = (s - 32) / 1.8;
+                    result = "°F  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°C";
                     break;
                 }
                 case R.id.btnFtoK:{
                     s = ((s - 32) / 1.8) + 273.15;
+                    result = "°F  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + " K";
                     break;
                 }
                 case R.id.btnKtoC:{
                     s = s - 273.15;
+                    result = " K  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°C";
                     break;
                 }
                 case R.id.btnKtoF: {
                     s = (s - 273.15) * 1.8 + 32;
+                    result = " K  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°F";
                     break;
                 }
             }
-            txtResult.setText(Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString());
+            txtResult.setText(result);
+            passrec = passrec + result;
         }
     };
 }
