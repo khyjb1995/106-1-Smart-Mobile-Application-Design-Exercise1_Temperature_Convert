d48d5a2049538885265a2454ace548b1610f6c02
khyjb1995 <yong950423@stu.xawl.edu.cn>
2017-11-08 20:33:08 +0800

    Done

--- app/src/main/java/com/example/khy_j/a410321171_exercise1_temperature_convert/MainActivity.java
+++ app/src/main/java/com/example/khy_j/a410321171_exercise1_temperature_convert/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.khy_j.a410321171_exercise1_temperature_convert;
 import android.icu.text.DecimalFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,51 +39,61 @@ public class MainActivity extends AppCompatActivity {
 
         txtPastResult.setText("");
         txtResult.setText("");
-        txtInput.setText("0");
     }
+
+
     private Button.OnClickListener myListener =new Button.OnClickListener(){
         @Override
         public void onClick(View view) {
-            Double s = Double.parseDouble(txtInput.getText().toString()) ;
-            if(txtResult.getText().toString() != "")
-            {
-                txtPastResult.setText(passrec + "\n" + txtPastResult.getText().toString());
+
+            if(txtInput.getText().length() != 0) {
+                Double s = Double.parseDouble(txtInput.getText().toString()) ;
+                if (txtResult.getText().toString() != "") {
+                    txtPastResult.setText(passrec + "\n" + txtPastResult.getText().toString());
+                }
+                passrec = s.toString();
+                switch (view.getId()) {
+                    case R.id.btnCtoF: {
+                        s = s * 1.8 + 32;
+                        result = "°C  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°F";
+                        break;
+                    }
+                    case R.id.btnCtoK: {
+                        s = s + 273.15;
+                        result = "°C  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + " K";
+                        break;
+                    }
+                    case R.id.btnFtoC: {
+                        s = (s - 32) / 1.8;
+                        result = "°F  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°C";
+                        break;
+                    }
+                    case R.id.btnFtoK: {
+                        s = ((s - 32) / 1.8) + 273.15;
+                        result = "°F  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + " K";
+                        break;
+                    }
+                    case R.id.btnKtoC: {
+                        s = s - 273.15;
+                        result = " K  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°C";
+                        break;
+                    }
+                    case R.id.btnKtoF: {
+                        s = (s - 273.15) * 1.8 + 32;
+                        result = " K  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°F";
+                        break;
+                    }
+                }
+                txtResult.setText(result);
+                passrec = passrec + result;
             }
-            passrec = s.toString();
-            switch (view.getId()) {
-                case R.id.btnCtoF: {
-                    s = s * 1.8 + 32;
-                    result = "°C  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°F";
-                    break;
-                }
-                case R.id.btnCtoK: {
-                    s = s + 273.15;
-                    result = "°C  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + " K";
-                    break;
-                }
-                case R.id.btnFtoC:{
-                    s = (s - 32) / 1.8;
-                    result = "°F  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°C";
-                    break;
-                }
-                case R.id.btnFtoK:{
-                    s = ((s - 32) / 1.8) + 273.15;
-                    result = "°F  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + " K";
-                    break;
-                }
-                case R.id.btnKtoC:{
-                    s = s - 273.15;
-                    result = " K  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°C";
-                    break;
-                }
-                case R.id.btnKtoF: {
-                    s = (s - 273.15) * 1.8 + 32;
-                    result = " K  to  " + Double.valueOf(Double.parseDouble(new DecimalFormat(".00").format(s))).toString() + "°F";
-                    break;
-                }
+            else {
+                Toast emptyMSG = Toast.makeText(MainActivity.this,"Please input numbers!",Toast.LENGTH_LONG);
+                emptyMSG.setGravity(Gravity.BOTTOM,0,200);
+                emptyMSG.show();
+
             }
-            txtResult.setText(result);
-            passrec = passrec + result;
+
         }
     };
 }
